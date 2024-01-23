ad033b5e1d5ff301aff786cf60337f0fb3b18931
Harsimran Singh <111746632+Harsimran726@users.noreply.github.com>
2024-01-23 09:16:16 +0530

    v4

--- project/settings.py
+++ project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'whitenoise.runserver_quiet', # new
+    #'whitenoise.runserver_quiet', # new
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app'
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # new
+    #'whitenoise.middleware.WhiteNoiseMiddleware', # new
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
